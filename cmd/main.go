24ad6f0407745baba80473237a83b35d9aaea67a
Venkat Pavan Munaganti <munaganti.v@northeastern.edu>
2023-10-03 23:05:29 -0400

    Changed to read default users from csv file

--- cmd/main.go
+++ cmd/main.go
@@ -1,12 +1,17 @@
 package main
 
 import (
+	"encoding/csv"
 	"fmt"
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
+	"io"
+	"os"
 	"webapp/config"
 	"webapp/db"
+	"webapp/models"
 	"webapp/routes"
+	"webapp/services"
 )
 
 func SetupGinRouter(db db.Database) *gin.Engine {
@@ -26,18 +31,70 @@ func init() {
 	}
 }
 
-func main() {
-	configs := config.GetConfigs()
-
-	d := &db.PostgresDB{}
-	err := d.InitDatabase(configs.DBConfig)
+func loadDefaultAccounts(defaultUsers config.DefaultUsers, s services.APIServices) {
+	file, err := os.Open(defaultUsers.Path)
 	if err != nil {
-		fmt.Printf("failed to initialize database: %s", err)
+		fmt.Println("Error opening file:", err)
+		return
+	}
+	defer func(file *os.File) {
+		err := file.Close()
+		if err != nil {
+			fmt.Println("Error closing file:", err)
+		}
+	}(file)
+
+	csvReader := csv.NewReader(file)
+
+	_, err = csvReader.Read()
+	if err != nil {
+		fmt.Println("Error reading header:", err)
+		return
 	}
 
-	r := SetupGinRouter(d)
+	for {
+		record, err := csvReader.Read()
+		if err != nil {
+			if err == io.EOF {
+				break
+			}
+			fmt.Println("Error reading record:", err)
+			return
+		}
 
-	err = r.Run(configs.ServerConfig.Host)
+		// Process the CSV record (record is a []string)
+		// You can access individual fields by index, e.g., record[0], record[1], etc.
+		fmt.Println(record)
+		passwordHash, hashError := s.AccountsService.HashPassword(record[3])
+		if hashError != nil {
+			fmt.Printf("Hashing password failed,\t%s\n", hashError)
+		} else {
+			account := models.Account{
+				FirstName: record[0],
+				LastName:  record[1],
+				Email:     record[2],
+				Password:  passwordHash,
+			}
+			accountCreationError := s.AccountsService.AddAccount(account)
+			if accountCreationError != nil {
+				fmt.Printf("Failed creating a default account,\t%s\n", accountCreationError)
+				continue
+			}
+			fmt.Printf("Successfully created a default account for %s", record[0])
+		}
+	}
+}
+
+func main() {
+	configs := config.GetConfigs()
+	s := services.APIServices{}
+	s.LoadServices(configs)
+	//Load default accounts
+	loadDefaultAccounts(configs.DefaultUsers, s)
+
+	r := SetupGinRouter(s.Database)
+
+	err := r.Run(configs.ServerConfig.Host)
 	if err != nil {
 		panic("failed to start Gin server: " + err.Error())
 	}
