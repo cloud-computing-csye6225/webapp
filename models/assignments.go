52a6087c24216c81300884c3053b97f7c7e03fd3
Venkat Pavan Munaganti <munaganti.v@northeastern.edu>
2023-10-03 23:02:11 -0400

    Created models for Account and Assignments

---
+++ models/assignments.go
@@ -0,0 +1,15 @@
+package models
+
+import "time"
+
+type Assignment struct {
+	ID                string    `gorm:"type:uuid;primaryKey;default:gen_random_uuid()" json:"id"`
+	Name              string    `json:"name" binding:"required"`
+	Points            int       `json:"points" binding:"required,gte=1,lte=100"`
+	NumOfAttempts     int       `json:"num_of_attempts" binding:"required,gte=1,lte=100"`
+	Deadline          time.Time `json:"deadline" binding:"required"`
+	AccountID         string    `json:"-"`
+	Account           Account   `gorm:"constraint:OnDelete:CASCADE" json:"-"`
+	AssignmentCreated time.Time `gorm:"autoCreateTime" json:"assignment_created"`
+	AssignmentUpdated time.Time `gorm:"autoUpdateTime" json:"assignment_updated"`
+}
