79f7c6a541a4f02aa8aca227f1dd8a1179795711
Venkat Pavan Munaganti <munaganti.v@northeastern.edu>
2023-11-07 16:08:50 -0500

    Updated statsd metric

--- routes/noRouteHandler.go
+++ routes/noRouteHandler.go
@@ -4,9 +4,11 @@ import (
 	"github.com/gin-gonic/gin"
 	"net/http"
 	"webapp/logger"
+	"webapp/utils"
 )
 
 func NoRouteHandler(context *gin.Context) {
+	utils.StatIncrement("InvalidRoutes", 1)
 	context.Header("Cache-Control", "no-store, no-cache, must-revalidate;")
 	logger.Warn("Invalid request")
 	context.String(http.StatusNotFound, "")
